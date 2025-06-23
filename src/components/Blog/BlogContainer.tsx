2f5fbb02d927f98b001325259c9f9ca664bfd2f5
thuonglh <thuonglh@dtp-education.com>
2025-06-23 17:03:33 +0700

    search post

--- src/components/Blog/BlogContainer.tsx
+++ src/components/Blog/BlogContainer.tsx
@@ -1,3 +1,4 @@
+
 import Breadcrumbs from "@/components/Breadcumbs/Breadcumbs";
 import ItemCardPost from "@/components/Post/ItemCardPost";
 import { ItemCardBlogProps } from "@/type/typeProps";
@@ -21,13 +22,13 @@ type BlogItemProp = {
   }[]
 }
 
-export default async function BlogContainer({
+const BlogContainer = ({
   itemPost,
   breadcrumbItem,
   title,
   description,
   dataCustom,
-}: BlogItemProp & CustomItemProps) {
+}: BlogItemProp & CustomItemProps) => {
   return (
     <div className="w-full pt-16 pb-20">
       <div className="max-w-(--max-width-desktop) mx-auto">
@@ -41,25 +42,26 @@ export default async function BlogContainer({
           </p>
         </div>
         {(dataCustom && dataCustom.length > 0) || (itemPost?.posts && itemPost.posts.length > 0) ? (
-            <div className="grid grid-cols-3 gap-4 gap-y-10 mt-28">
-              {(dataCustom ? dataCustom : itemPost?.posts)?.map((post: ItemCardBlogProps, key: number) => (
-                <ItemCardPost
-                  key={key}
-                  title={post.title}
-                  image={post.image}
-                  description={post.description}
-                  excerpt={post.excerpt}
-                  slug={post.slug}
-                  createdAt={post.createdAt}
-                  author={post.author}
-                  category={post.category}
-                />
-              ))}
-            </div>
-          ) : (
-            <NotFoundBlog/>
-          )}
+          <div className="grid grid-cols-3 gap-4 gap-y-10 mt-28">
+            {(dataCustom ? dataCustom : itemPost?.posts)?.map((post: ItemCardBlogProps, key: number) => (
+              <ItemCardPost
+                key={key}
+                title={post.title}
+                image={post.image}
+                description={post.description}
+                excerpt={post.excerpt}
+                slug={post.slug}
+                createdAt={post.createdAt}
+                author={post.author}
+                category={post.category}
+              />
+            ))}
+          </div>
+        ) : (
+          <NotFoundBlog />
+        )}
       </div>
     </div>
   )
 }
+export default BlogContainer
