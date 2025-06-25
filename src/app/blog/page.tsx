8fafbbfd4a108b1e3e3532721606149fdf60ba21
thuonglh <thuonglh@dtp-education.com>
2025-06-25 15:55:50 +0700

    add Suspense

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import BlogPage from "@/containers/Blog/BlogPage";
+import { Suspense } from "react";
 
 const Blog = () => {
 
   return (
-    <BlogPage/>
+    <Suspense fallback={<div>loading...</div>}>
+      <BlogPage/>
+    </Suspense>
   )
 }
 export default Blog
