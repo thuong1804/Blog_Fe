08934d8ca2385ea75603d295235998e2325d4339
thuonglh <thuongtvt30@gmail.com>
2026-01-13 15:41:18 +0700

    update gg

--- src/app/api/auth/callback/google/route.ts
+++ src/app/api/auth/callback/google/route.ts
@@ -14,8 +14,8 @@ export async function GET(req: NextRequest) {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams({
                 code,
-                client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-                client_secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+                client_id: process.env.GOOGLE_CLIENT_ID!,
+                client_secret: process.env.GOOGLE_CLIENT_SECRET!,
                 redirect_uri: `${process.env.NEXT_PUBLIC_URL_BLOG}/api/auth/callback/google`,
                 grant_type: "authorization_code",
             }),
