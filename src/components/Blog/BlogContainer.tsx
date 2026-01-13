ae36056a6f8ed487324ee72013dcf3d75f1607d0
thuonglh <thuongtvt30@gmail.com>
2026-01-13 13:18:12 +0700

    update

--- src/components/Blog/BlogContainer.tsx
+++ src/components/Blog/BlogContainer.tsx
@@ -96,7 +96,6 @@ const BlogContainer = ({
                             className="join-item btn"
                             disabled={currentPage === 1}
                             onClick={() => handleChangePage?.(currentPage - 1)}
-                            onClick={() => handleChangePage?.(currentPage - 1)}
                         >
                             «
                         </button>
@@ -110,7 +109,6 @@ const BlogContainer = ({
                                     className={`join-item btn ${pageNumber === currentPage ? "btn-active" : ""
                                         }`}
                                     onClick={() => handleChangePage?.(pageNumber)}
-                                    onClick={() => handleChangePage?.(pageNumber)}
                                 >
                                     {pageNumber}
                                 </button>
@@ -120,7 +118,6 @@ const BlogContainer = ({
                         <button
                             className="join-item btn"
                             disabled={currentPage === totalPages}
-                            onClick={() => handleChangePage?.(currentPage + 1)}
                             onClick={() => handleChangePage?.(currentPage + 1)}
                         >
                             »
