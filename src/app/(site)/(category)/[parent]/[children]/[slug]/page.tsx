236abfb19dbceba11d25f469f6ea3698930367c8
thuonglh <thuongtvt30@gmail.com>
2026-01-22 16:18:34 +0700

    test cache

--- src/app/(site)/(category)/[parent]/[children]/[slug]/page.tsx
+++ src/app/(site)/(category)/[parent]/[children]/[slug]/page.tsx
@@ -16,6 +16,8 @@ export async function generateStaticParams() {
 
     if (!data || !data.postAllSlugs) return [];
 
+    return [{ slug: 'backend' }, { slug: 'fullstack-development' }];
+
     return data.postAllSlugs.map((post: PostSlugData) => {
         const parentSlug = post.category?.parent?.slug || "general";
         const childrenSlug = post.category?.slug || "post";
