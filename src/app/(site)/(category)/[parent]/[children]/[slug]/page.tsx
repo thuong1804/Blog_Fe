32f4b7908ce9c410beabc2d9b55a379147a9b8b0
thuonglh <thuongtvt30@gmail.com>
2026-01-22 15:28:10 +0700

    update

--- src/app/(site)/(category)/[parent]/[children]/[slug]/page.tsx
+++ src/app/(site)/(category)/[parent]/[children]/[slug]/page.tsx
@@ -14,8 +14,7 @@ export async function generateStaticParams() {
     });
 
     if (!data || !data.postAllSlugs) return [];
-
-return [{ parent: 'web-development', children: 'fullstack', slug: 'fullstack-development' }];
+    
 
     return data.postAllSlugs.map((post: PostSlugData) => {
         const parentSlug = post.category?.parent?.slug || "general";
